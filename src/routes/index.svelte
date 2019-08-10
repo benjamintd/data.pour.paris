2c9cd3d82f884baa1f8cb2fbbdd68a75ece01d9c
Benjamin TD <benjamin.tdm@gmail.com>
2019-08-10 10:12:35 +0200

    fonts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,22 +3,32 @@
   import Github from "../components/Github.svelte";
 </script>
 
+<style>
+  .logotype {
+    font-family: "Bree Serif", serif;
+  }
+
+  .logo {
+    margin-bottom: -0.7rem;
+  }
+</style>
+
 <svelte:head>
   <title>Open Data Paris</title>
 </svelte:head>
 
 <div class="flex flex-column">
+
   <div
-    class="shadow-1 w-100 ph4 pv2 flex flex-row-l flex-row-m flex-column
-    items-center">
+    class="bg-near-white shadow-1 w-100 ph4 pv2 flex flex-row-l flex-row-m
+    flex-column items-center">
     <a href="/">
-      <div class="w2 h2 mr3-l mr3-m">
-        <Logo />
+      <div class="w2 h2 mr3-l mr3-m logo">
+        <Logo fill="black" />
       </div>
     </a>
-    <div>
-      <h1 class="f1-l f1-m f2 mv2">data.pour.paris</h1>
-    </div>
+    <span class="logotype f1-l f1-m f2 mv2">data.pour.paris</span>
+
     <div class="ml-auto-l ml-auto-m">
       <Github />
     </div>
